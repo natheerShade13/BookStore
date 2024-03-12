9cc428f3c3e09ccbfed2913eee83ac6e44b1ad65
Chadwin Kyle Fritz <100412900+ChadwinFritz@users.noreply.github.com>
2024-03-12 22:11:02 +0200

    Update IPaymentRepository.java

--- src/main/java/za/ac/cput/repository/IPaymentRepository.java
+++ src/main/java/za/ac/cput/repository/IPaymentRepository.java
@@ -4,11 +4,10 @@ import za.ac.cput.domain.Payment;
 
 import java.util.List;
 
-// IPaymentRepository.java
 public interface IPaymentRepository {
-    List<Payment> getAll();
     Payment create(Payment payment);
     Payment read(String paymentID);
     Payment update(Payment payment);
     boolean delete(String paymentID);
+    List<Payment> getAll();
 }
