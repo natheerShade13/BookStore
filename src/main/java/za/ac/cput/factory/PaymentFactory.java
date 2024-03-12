07f5a28c0203a7cdd123d45257c57a5c92e172a0
Chadwin Kyle Fritz <100412900+ChadwinFritz@users.noreply.github.com>
2024-03-12 22:07:07 +0200

    Update PaymentFactory.java

--- src/main/java/za/ac/cput/factory/PaymentFactory.java
+++ src/main/java/za/ac/cput/factory/PaymentFactory.java
@@ -1,12 +1,16 @@
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.Payment;
-
 import java.util.Date;
 
-// PaymentFactory.java
 public class PaymentFactory {
     public static Payment createPayment(String paymentID, String orderID, double amount, Date paymentDate, String paymentMethod) {
-        return new Payment(paymentID, orderID, amount, paymentDate, paymentMethod);
+        return new Payment.Builder()
+                .paymentID(paymentID)
+                .orderID(orderID)
+                .amount(amount)
+                .paymentDate(paymentDate)
+                .paymentMethod(paymentMethod)
+                .build();
     }
 }
