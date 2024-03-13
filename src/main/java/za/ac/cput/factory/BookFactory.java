c86e396a1423e51011423c77500889bb207bc86a
Natheer13 <natheer.shade@gmail.com>
2024-03-14 00:19:14 +0200

    Book, BookFactory, Helper (Updated).

--- src/main/java/za/ac/cput/factory/BookFactory.java
+++ src/main/java/za/ac/cput/factory/BookFactory.java
@@ -1,4 +1,36 @@
 package za.ac.cput.factory;
 
+
+import za.ac.cput.domain.Book;
+import za.ac.cput.util.Helper;
+
+/*
+
+Author: Natheer Shade 217159109 13/03/2024
+
+ */
 public class BookFactory {
+
+    public static Book buildBook(String bookID, String title, String author, String genre, int price
+            , int stockQuant){
+        if(Helper.isNullOrEmpty(bookID) || Helper.isNullOrEmpty(title) || Helper.isNullOrEmpty(author)
+                || Helper.isNullOrEmpty(genre) || Helper.isNegative(price) || Helper.isNegative(stockQuant) )
+            return null;
+
+        return new Book.Builder().setBookId(bookID).setTitle(title).setAuthor(author).setGenre(genre)
+                .setPrice(price).setStockQaunt(stockQuant).build();
+    }
+
+    public static Book buildBook(String title, String author, String genre, int price, int stockQuant){
+        if(Helper.isNullOrEmpty(title) || Helper.isNullOrEmpty(author) || Helper.isNullOrEmpty(genre)
+                || Helper.isNegative(price) || Helper.isNegative(stockQuant) )
+            return null;
+
+        String bookNum = Helper.generateID();
+
+        return new Book.Builder().setBookId(bookNum).setTitle(title).setAuthor(author).setGenre(genre)
+                .setPrice(price).setStockQaunt(stockQuant).build();
+    }
+
+
 }
--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -2,10 +2,21 @@ package za.ac.cput.util;
 
 import java.util.UUID;
 
+/*
+
+Author: Natheer Shade 217159109 13/03/2024
+
+ */
 public class Helper {
 
     public static boolean isNullOrEmpty(String a){
         if(a == null || a.isEmpty())
+            return true;
+        return false;
+    }
+
+    public static boolean isNegative(int b){
+        if(b < 0)
             return true;
         return false;
     }
