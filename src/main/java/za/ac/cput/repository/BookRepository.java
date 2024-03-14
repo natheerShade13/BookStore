9ce4e4ee47d568e02a9aedbbc54dbdb5ffb42d2b
Natheer13 <natheer.shade@gmail.com>
2024-03-14 02:56:30 +0200

    BookRepositoryTest.

--- src/main/java/za/ac/cput/repository/BookRepository.java
+++ src/main/java/za/ac/cput/repository/BookRepository.java
@@ -2,31 +2,72 @@ package za.ac.cput.repository;
 
 import za.ac.cput.domain.Book;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookRepository implements IBookRepository{
+
+    private static IBookRepository repository = null;
+
+    private List<Book> booksList;
+
+    private BookRepository(){
+        booksList = new ArrayList<Book>();
+    }
+
+    public static IBookRepository getRepository(){
+        if(repository == null)
+            repository = new BookRepository();
+
+        return repository;
+    }
+
     @Override
     public List<Book> getAll() {
-        return null;
+
+        return booksList;
     }
 
     @Override
     public Book create(Book book) {
+        boolean success = booksList.add(book);
+        if(success){
+            return book;
+        }
         return null;
     }
 
     @Override
     public Book read(String s) {
+        for(Book e : booksList){
+            if(e.getBookID().equals(s))
+                return e;
+        }
         return null;
     }
 
     @Override
     public Book update(Book book) {
+        String id = book.getBookID();
+        Book bookOld = read(id);
+
+        if(bookOld == null){
+            return null;
+        }
+        boolean success = delete(id);
+        if(success){
+            booksList.add(book);
+            return book;
+        }
         return null;
     }
 
     @Override
     public boolean delete(String s) {
-        return false;
+        Book bookToDelete = read(s);
+        if(bookToDelete == null){
+            return false;
+        }
+        return (booksList.remove(bookToDelete));
     }
 }
