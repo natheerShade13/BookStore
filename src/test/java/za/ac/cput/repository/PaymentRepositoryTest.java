f5223289181b5dad5104c191ca540e0ea2edb37b
Chadwin Kyle Fritz <100412900+ChadwinFritz@users.noreply.github.com>
2024-03-15 18:36:56 +0200

    Update PaymentRepositoryTest.java

--- src/test/java/za/ac/cput/repository/PaymentRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/PaymentRepositoryTest.java
@@ -20,88 +20,51 @@ Author: Chadwin Kyle Fritz 218068360 15/03/2024
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PaymentRepositoryTest {
 
-    private static IPaymentRepository repository = new PaymentRepository();
+    private static IPaymentRepository repository = PaymentRepository.getRepository();
 
-    private static final Payment paymentA = PaymentFactory.createPayment("987654", 500, new Date(), "Credit Card");
+    private final Payment paymentA = PaymentFactory.createPayment("987654", 500, new Date(), "Credit Card");
 
-    private static final Payment paymentB = PaymentFactory.createPayment("456789", 1000, new Date(), "Debit Card");
+    @Test
+    @Order(4)
+    void getAll() {
+        System.out.println(repository.getAll());
+    }
 
     @Test
     @Order(1)
-    void testCreatePaymentA() {
-        Payment createdPayment = repository.create(paymentA);
-        assertNotNull(createdPayment);
-        assertEquals(paymentA, createdPayment);
-        System.out.println("Created Payment A: " + createdPayment);
+    void create() {
+        Payment add = repository.create(paymentA);
+        assertNotNull(add);
+        System.out.println(add);
     }
 
     @Test
     @Order(2)
-    void testCreatePaymentB() {
-        Payment createdPayment = repository.create(paymentB);
-        assertNotNull(createdPayment);
-        assertEquals(paymentB, createdPayment);
-        System.out.println("Created Payment B: " + createdPayment);
+    void read() {
+        String paymentID = paymentA.getPaymentID();
+        Payment read = repository.read(paymentID);
+        assertNotNull(read);
+        System.out.println(read);
     }
 
     @Test
     @Order(3)
-    void testReadPayment() {
-        Payment readPaymentA = repository.read(paymentA.getPaymentID());
-        assertNotNull(readPaymentA);
-        assertEquals(paymentA, readPaymentA);
-        System.out.println("Read Payment A: " + readPaymentA);
-
-        Payment readPaymentB = repository.read(paymentB.getPaymentID());
-        assertNotNull(readPaymentB);
-        assertEquals(paymentB, readPaymentB);
-        System.out.println("Read Payment B: " + readPaymentB);
-    }
-
-    @Test
-    @Order(4)
-    void testUpdatePayment() {
-        Payment updatedPaymentA = new Payment.Builder()
-                .setPaymentID(paymentA.getPaymentID())
-                .setAmount(500)
-                .setPaymentDate(new Date())
-                .setPaymentMethod("Debit Card")
+    void update() {
+        Payment newPayment = new Payment.Builder()
+                .paymentID(paymentA.getPaymentID())
+                .amount(750)
+                .paymentDate(new Date())
+                .paymentMethod("Debit Card")
                 .build();
-
-        Payment updatedPaymentB = new Payment.Builder()
-                .setPaymentID(paymentB.getPaymentID())
-                .setAmount(1000)
-                .setPaymentDate(new Date())
-                .setPaymentMethod("Credit Card")
-                .build();
-
-        Payment resultA = repository.update(updatedPaymentA);
-        assertNotNull(resultA);
-        assertEquals(updatedPaymentA, resultA);
-        System.out.println("Updated Payment A: " + resultA);
-
-        Payment resultB = repository.update(updatedPaymentB);
-        assertNotNull(resultB);
-        assertEquals(updatedPaymentB, resultB);
-        System.out.println("Updated Payment B: " + resultB);
+        Payment update = repository.update(newPayment);
+        assertNotNull(update);
+        System.out.println(update);
     }
 
     @Test
     @Order(5)
-    void testDeletePayment() {
+    void delete() {
         assertTrue(repository.delete(paymentA.getPaymentID()));
-        System.out.println("Deleted Payment A: " + paymentA.getPaymentID());
-
-        assertTrue(repository.delete(paymentB.getPaymentID()));
-        System.out.println("Deleted Payment B: " + paymentB.getPaymentID());
-    }
-
-    @Test
-    @Order(6)
-    void testGetAllPayments() {
-        repository.create(paymentA);
-        repository.create(paymentB);
-        assertNotNull(repository.getAll());
-        System.out.println("All Payments: " + repository.getAll());
+        System.out.println("Successfully deleted");
     }
 }
