803220a0446d6a5b36f0111ce4d05f4264c94f4f
Keitumetse <keitudimpe@gmail.com>
2024-03-15 19:05:21 +0200

    customer POJo CustomerFactory CustomerFactoryTest CustomerRepository CustomerRepositoryTest ICustomerRepository

--- src/main/java/za/ac/cput/repository/IBookRepository.java
+++ src/main/java/za/ac/cput/repository/IBookRepository.java
@@ -1,10 +1,11 @@
 package za.ac.cput.repository;
 
 import za.ac.cput.domain.Book;
+import za.ac.cput.domain.Order;
 
 import java.util.List;
 
-public interface IBookRepository extends IRepository<Book, String> {
+public interface IBookRepository extends IRepository<Book, String>{
 
     List<Book> getAll();
 
