ac2ec2fea3c136fc803ef27b98c3dcdc752e3726
Keitumetse <keitudimpe@gmail.com>
2024-03-15 19:05:21 +0200

    customer POJo CustomerFactory CustomerFactoryTest CustomerRepository CustomerRepositoryTest ICustomerRepository

--- src/main/java/za/ac/cput/repository/ICustomerRepository.java
+++ src/main/java/za/ac/cput/repository/ICustomerRepository.java
@@ -6,6 +6,18 @@ import java.util.List;
 
 public interface ICustomerRepository extends IRepository<Customer, String>{
 
+    @Override
+    Customer create(Customer customer);
+
+    @Override
+    Customer read(String s);
+
+    @Override
+    Customer update(Customer customer);
+
+    @Override
+    boolean delete(String s);
+
     List<Customer> getAll();
 
 }
---
+++ src/test/java/za/ac/cput/factory/CustomerFactoryTest.java
@@ -0,0 +1,30 @@
+package za.ac.cput.factory;
+
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Customer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerFactoryTest {
+
+    @Test
+    void TestBuildCustomer() {
+        Customer c= CustomerFactory.buildCustomer("3456","keitu",
+                "dimpe","kafhm@gmail.com","143 lowry street",
+                "+27 67 789 9875");
+        assertNotNull(c);
+        System.out.println(c.toString());
+
+    }
+    @Test
+    void testBuildCustomerWithFail(){
+        Customer c2= CustomerFactory.buildCustomer("Leago",
+                "Maphothoma","KarinLeaMH@gmail.com","11829 Tima Street",
+                "+27 84 995 9282");
+        assertNotNull(c2);
+        System.out.println(c2);
+
+    }
+
+}
