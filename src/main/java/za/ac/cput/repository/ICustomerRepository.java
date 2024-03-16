1d7be29031e345cfc5bfa15708e3102440354a89
natheerShade13 <129689662+natheerShade13@users.noreply.github.com>
2024-03-16 15:23:08 +0200

    Update ICustomerRepository.java

--- src/main/java/za/ac/cput/repository/ICustomerRepository.java
+++ src/main/java/za/ac/cput/repository/ICustomerRepository.java
@@ -6,18 +6,6 @@ import java.util.List;
 
 public interface ICustomerRepository extends IRepository<Customer, String>{
 
-    @Override
-    Customer create(Customer customer);
-
-    @Override
-    Customer read(String s);
-
-    @Override
-    Customer update(Customer customer);
-
-    @Override
-    boolean delete(String s);
-
     List<Customer> getAll();
 
 }
