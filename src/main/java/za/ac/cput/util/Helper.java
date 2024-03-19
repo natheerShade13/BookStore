0c701dbb4dbc1b07074b30b936a8214491d7fc5e
natheerShade13 <129689662+natheerShade13@users.noreply.github.com>
2024-03-19 13:37:07 +0200

    Update Helper.java

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -20,11 +20,6 @@ public class Helper {
             return true;
         return false;
     }
-    public static boolean isNegative(int b){
-        if(b < 0)
-            return true;
-        return false;
-    }
 
     public static String generateID(){
         return UUID.randomUUID().toString();
