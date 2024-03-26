85211c178b4d5c1b383383c068336ab33a370e19
natheerShade13 <129689662+natheerShade13@users.noreply.github.com>
2024-03-26 19:01:34 +0200

    Updated all the Book classes. (#36)

--- src/main/java/za/ac/cput/repository/CustomerRepository.java
+++ src/main/java/za/ac/cput/repository/CustomerRepository.java
@@ -6,6 +6,14 @@ import za.ac.cput.domain.Customer;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+Customer.java
+Customer model class
+Author: Fereshteh Keitumetse Dimpe 221806229
+https://github.com/Keitudimps/BookStore
+Date: 24 March 2024
+ */
+
 public class CustomerRepository implements ICustomerRepository {
     private static ICustomerRepository repository = null;
     private List<Customer> customerList;
--- src/main/java/za/ac/cput/repository/ICustomerRepository.java
+++ src/main/java/za/ac/cput/repository/ICustomerRepository.java
@@ -4,6 +4,14 @@ import za.ac.cput.domain.Customer;
 
 import java.util.List;
 
+/*
+Customer.java
+Customer model class
+Author: Fereshteh Keitumetse Dimpe 221806229
+https://github.com/Keitudimps/BookStore
+Date: 24 March 2024
+ */
+
 public interface ICustomerRepository extends IRepository<Customer, String>{
 
     List<Customer> getAll();
